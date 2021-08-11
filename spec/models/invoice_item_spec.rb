5d9e71bf3b57be422ec4b79fcccc05a8bb5cba8a
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-10 18:05:52 -0600

    Test for discounted revenue

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to(:item) }
     it { should belong_to(:invoice) }
-    # it { should have_many(:) }
-    # it { should have_many(:).through(:) }
   end
 
   describe 'enum status' do
@@ -28,18 +26,13 @@ RSpec.describe InvoiceItem, type: :model do
         expect(expected).to eq(@invoice_item1)
       end
     end
+  end
 
-    describe '.total_revenue' do
-      it 'calculates total revenue of a collection of invoice items' do
-        expected = @invoice1.invoice_items.total_revenue
-
-        expect(expected).to eq(78750)
+  describe 'instance methods' do
+    describe '#discount_revenue' do
+      it 'gives the discounted revenue for an invoice_item' do
+        expect(@invoice_item1.discount_revenue).to eq(@discount3)
       end
     end
   end
-
-  # describe 'instance methods' do
-  #   describe '#' do
-  #   end
-  # end
 end
