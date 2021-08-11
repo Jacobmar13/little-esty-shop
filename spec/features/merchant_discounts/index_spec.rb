1bcfdaf76eef566c15f1d8ec0f41158f6a1f7eea
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-10 19:28:17 -0600

    Fully test out index page

--- spec/features/merchant_discounts/index_spec.rb
+++ spec/features/merchant_discounts/index_spec.rb
@@ -3,8 +3,34 @@ require 'rails_helper'
 RSpec.describe 'the merchant discounts index' do
 
   describe 'display' do
-    it 'shows header text' do
+    it 'shows merchant and discounts' do
       visit merchant_discounts_path(@merchant1.id)
+
+      expect(page).to have_content(@merchant1.name)
+      expect(page).to have_content(@discount1.quantity)
+      expect(page).to have_content(@discount1.discount)
+    end
+
+    it 'has clickable links/buttons' do
+      visit merchant_discounts_path(@merchant1.id)
+
+      expect(page).to have_button('New Discount')
+      expect(page).to have_button('View Discount')
+      expect(page).to have_button('Delete Discount')
+    end
+  end
+
+  describe 'actions' do
+    it 'can delete a discount' do
+      visit merchant_discounts_path(@merchant1.id)
+
+      expect(page).to have_content(@discount1.quantity)
+      expect(page).to have_content(@discount1.discount)
+
+      first(:button, 'Delete Discount').click
+
+      expect(current_path).to eq(merchant_discounts_path(@merchant1.id))
+      expect(page).to_not have_content(@discount1.quantity)
     end
   end
 end
