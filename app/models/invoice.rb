87b9bcd08f9d7558be1d0b3ce5ac8d5b23c44523
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-10 18:07:16 -0600

    Make discounted revenue method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,11 +11,21 @@ class Invoice < ApplicationRecord
     invoice_items.sum('quantity * unit_price')
   end
 
+  def discount_revenue
+    revenue = 0
+    invoice_items.each do |discount|
+      if discount.discount_revenue != nil
+        revenue += ((discount.discount_revenue.discount.to_f / 100) * discount.total_revenue)
+      end
+    end
+    total_revenue - revenue
+  end
+
   def self.incomplete_invoices_by_date
     joins(:invoice_items)
-    .where.not(invoice_items: {status: :shipped})
-    .select('invoices.*')
-    .group(:id)
-    .order(:created_at)
+      .where.not(invoice_items: {status: :shipped})
+      .select('invoices.*')
+      .group(:id)
+      .order(:created_at)
   end
 end
